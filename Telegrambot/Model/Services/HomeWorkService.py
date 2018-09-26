548301132d01494911183edbee6ed22fcb9b7718
Olo21 <39828786+Olo21@users.noreply.github.com>
2018-09-26 19:40:41 +0300

    add get all home works method

--- Telegrambot/Model/Services/HomeWorkService.py
+++ Telegrambot/Model/Services/HomeWorkService.py
@@ -9,7 +9,18 @@ class HomeWorkService(BaseService):
         pass
 
     def add_hw(self,name, description):
+            # НАЗВАНИЕ description
+            # КОЛИЧЕСТВО ДОМАШЕК
+            # добавить колонку 
         return "метод в сервисе пустой"
 
+    def get_all_hw(self):
+         sql = 'SELECT * FROM homeworks;'
+         hw = self.db.execute_select(sql)
+         if len(hw) > 0:  return hw;
+
+         return None
+
+     
 
 
