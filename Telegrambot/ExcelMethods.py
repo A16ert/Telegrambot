64a1848edc37729b255614c5c4ee630d50dfba90
Albert <fajb420@gmail.com>
2018-09-27 16:43:22 +0300

    add excel service

---
+++ Telegrambot/ExcelMethods.py
@@ -0,0 +1,5 @@
+from Model.Services.ExcelService import ExcelService
+
+
+def get_all_home_works(bot, id):
+    pass
---
+++ Telegrambot/Model/Services/ExcelService.py
@@ -0,0 +1,15 @@
+from ..db.SQLighter import SQLighter
+from .BaseService import BaseService
+
+class ExcelService(BaseService):
+    """description of class"""
+
+    def __init__(self):
+       BaseService.__init__(self)
+       pass
+
+    def get_all_home_works(self):
+
+        pass
+
+
